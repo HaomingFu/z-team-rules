9230adad99f219793a55a0310ccb27fc92538245
gnijuohz <gnijuohz@gmail.com>
2014-04-20 13:06:46 -0600

    partially

--- week2/best_time_to_buy_and_sell.py
+++ week2/best_time_to_buy_and_sell.py
@@ -4,6 +4,10 @@ Author: Jing Zhou
 Date: 3/22, 2014
 Version: 3(first 2 TLE, the last one passed ok)
 Time complexity: O(n), iterate list 4 times
+Lesson learned: the best way to reversed a sequence(list or string) is by [::-1].
+list(reversed()) is much slower so it should be avoided.
+nl = list(pre_list) nl.reverse() is the second best approach
+See: http://stackoverflow.com/a/3705705/1062364
 """
 def maxProfit(prices):
     if not prices:
--- week2/reverse_words_in_a_string.py
+++ week2/reverse_words_in_a_string.py
@@ -6,4 +6,7 @@ class Solution:
     # @param s, a string
     # @return a string
     def reverseWords(self, s):
-        return ' '.join([word for word in reversed(s.split())])
+        return ' '.join(s.split()[::-1]) # This is the fastest
+        #return ' '.join(reversed(s.split())) #second best
+        #this one also passed the test but not very good
+        #return ' '.join([word for word in reversed(s.split())])
